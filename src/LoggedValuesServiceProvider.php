3a9a7cb3f36d5ba0261acd0157c92f4ee482410a
JayAhr <sgjohann@gmail.com>
2020-10-27 20:18:10 +0100

    Update LoggedValuesServiceProvider.php

--- src/LoggedValuesServiceProvider.php
+++ src/LoggedValuesServiceProvider.php
@@ -15,7 +15,7 @@ class LoggedValuesServiceProvider extends ServiceProvider
     {
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'legalworks');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'legalworks');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         // Publishing is only necessary when using the CLI.
